9fc2cbf4feafd3a6ebc37ce07295574b7b8b920e
Denize Ignacio <denizepatricignacio@gmail.com>
2023-02-04 19:49:05 -0800

    Update textView to "Done" and play sound indicating timer has finished after button click

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+
+    let eggTimes = ["Soft": 2, "Medium": 420, "Hard": 720]
+    var secondsRemaining = 60
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer?
     
+    @IBOutlet weak var eggsView: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progressView.progress = 0.0
+        secondsPassed = 0
+        eggsView.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updatedTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updatedTimer() {
+        if secondsPassed < totalTime {
+            print(Float(secondsPassed) / Float(totalTime))
+            progressView.progress = Float(secondsPassed) / Float(totalTime)
+            secondsPassed += 1
+        } else {
+            print(Float(secondsPassed) / Float(totalTime))
+            progressView.progress = Float(secondsPassed) / Float(totalTime)
+            timer.invalidate()
+            eggsView.text = "Done"
+            playSound()
+        }
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
 
-
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            guard let player = player else { return }
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
